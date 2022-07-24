3350f4e9646511865bd52adfff9513f02fa26e7e
minty <jhkieha@gmail.com>
2022-07-24 17:14:24 -0400

    copied exmaple code for sending push notifications from expos docs. Will pull this apart and refactor once i understand it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
+import * as Notifications from 'expo-notifications';
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Button } from 'react-native';
 
 import { styles } from 'styles';
 import { ImageContainer } from 'components/imageContainer';
-import { fetchCat, notify } from 'utils';
+import { fetchCat, notify, registerForPushNotificationsAsync, sendPushNotification } from 'utils';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 function App() {
   const [imageUri, setImageUri] = React.useState<string>(null);
+  const [expoPushToken, setExpoPushToken] = React.useState<string>(null);
+  const [notification, setNotification] = React.useState<Notifications.Notification>(null);
+  const notificationListener = React.useRef<any>();
+  const responseListener = React.useRef<any>();
+
+  React.useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => setExpoPushToken(token));
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   async function handleClick() {
     const imageUri = await fetchCat();
@@ -16,6 +48,11 @@ function App() {
     if (imageUri) {
       setImageUri(imageUri);
       notify('Got a Cat for the Day!');
+      sendPushNotification(expoPushToken, {
+        title: 'Got a Cat!',
+        body: 'Youve received your cat',
+        data: { extra: 'Get more cats tomorrow!' },
+      });
     }
   }
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 export { default as fetchCat } from './fetchCat';
 export { default as notify } from './notify';
+export { default as registerForPushNotificationsAsync } from './registerPushNotifications';
+export { default as sendPushNotification } from './sendPushNotification';
