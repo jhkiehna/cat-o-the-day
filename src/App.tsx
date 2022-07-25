380db8efd9a9890cfbeea1c9014e6e624109f4cc
minty <jhkieha@gmail.com>
2022-07-25 17:54:07 -0400

    refactored to: remove unneeded extra component. pull random images off array, and discard empty images. Not show the webview containing google images

--- src/App.tsx
+++ src/App.tsx
@@ -8,45 +8,37 @@ import { WebViewMessageEvent } from 'react-native-webview/lib/WebViewTypes';
 import { styles } from 'styles';
 import { ImageContainer } from 'components/imageContainer';
 
-function ImageWebView(props: { setImageUri: Function }) {
-  const { setImageUri } = props;
-
-  function handleOnMessage(event: WebViewMessageEvent) {
-    const imageSrcs: string[] = JSON.parse(event.nativeEvent.data);
-    console.log({ imageSrcs });
-    const randomImageSrc = imageSrcs[Math.floor(Math.random() * imageSrcs.length)];
-    setImageUri(randomImageSrc);
-  }
-
-  return (
-    <View style={{ width: 100, height: 100, borderColor: 'red', borderWidth: 1, borderStyle: 'solid' }}>
-      <WebView
-        source={{ uri: 'https://www.google.com/search?q=cats&tbm=isch&tbs=isz:m' }}
-        injectedJavaScript={`(() => {let imageSrcs = []; window.document.querySelectorAll('img').forEach((img) => {imageSrcs = [...imageSrcs, img.src];});window.ReactNativeWebView.postMessage(JSON.stringify(imageSrcs));})()`}
-        onMessage={handleOnMessage}
-      />
-    </View>
-  );
-}
-
 function App() {
   const [imageUri, setImageUri] = React.useState<string>(null);
-  const [loadWebView, setLoadWebView] = React.useState(false);
 
-  React.useEffect(() => {
-    const timeout = setTimeout(() => setLoadWebView(true), 500);
+  function handleOnMessage(event: WebViewMessageEvent) {
+    if (imageUri) return;
 
-    return () => clearTimeout(timeout);
-  }, []);
+    const imageSrcs: string[] = JSON.parse(event.nativeEvent.data);
+    console.log({ imageSrcs });
+
+    while (imageSrcs.length) {
+      const randomImageSrc = imageSrcs.splice(Math.floor(Math.random() * imageSrcs.length), 1)?.pop();
+      console.log(randomImageSrc);
+      if (randomImageSrc?.length) {
+        setImageUri(randomImageSrc);
+        break;
+      }
+    }
+  }
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <WebView
+        style={{ display: 'none', width: 0, height: 0 }}
+        source={{ uri: 'https://www.google.com/search?q=cats&tbm=isch&tbs=isz:m' }}
+        injectedJavaScript={`(() => {let imageSrcs = []; window.document.querySelectorAll('img').forEach((img) => {imageSrcs = [...imageSrcs, img.src];});window.ReactNativeWebView.postMessage(JSON.stringify(imageSrcs));})()`}
+        onMessage={handleOnMessage}
+      />
       <Text>Cat-o-the-day App</Text>
 
       {imageUri ? <ImageContainer imageUri={imageUri} /> : null}
-
-      {loadWebView ? <ImageWebView setImageUri={setImageUri} /> : null}
     </View>
   );
 }
