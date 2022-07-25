148f98fc90098a0e2a79a267e63ec3a221947f8b
minty <jhkieha@gmail.com>
2022-07-25 18:22:02 -0400

    refactored to remove that dangerous while loop. Just throwing all images into an array and saving. added the button back to randomly grab an image from the array

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { WebViewMessageEvent } from 'react-native-webview/lib/WebViewTypes';
 
@@ -9,36 +9,36 @@ import { styles } from 'styles';
 import { ImageContainer } from 'components/imageContainer';
 
 function App() {
-  const [imageUri, setImageUri] = React.useState<string>(null);
+  const [displayedImage, setDisplayedImage] = React.useState<string>(null);
+  const [images, setImages] = React.useState<string[]>([]);
 
   function handleOnMessage(event: WebViewMessageEvent) {
-    if (imageUri) return;
+    if (images.length) return; // This fires twice for some reason, returning early if we already have images
 
-    const imageSrcs: string[] = JSON.parse(event.nativeEvent.data);
+    const imageSrcs: string[] = JSON.parse(event.nativeEvent.data).filter((src: string) => src.length);
     console.log({ imageSrcs });
 
-    while (imageSrcs.length) {
-      const randomImageSrc = imageSrcs.splice(Math.floor(Math.random() * imageSrcs.length), 1)?.pop();
-      console.log(randomImageSrc);
-      if (randomImageSrc?.length) {
-        setImageUri(randomImageSrc);
-        break;
-      }
-    }
+    setImages(imageSrcs);
   }
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
       <WebView
         style={{ display: 'none', width: 0, height: 0 }}
         source={{ uri: 'https://www.google.com/search?q=cats&tbm=isch&tbs=isz:m' }}
         injectedJavaScript={`(() => {let imageSrcs = []; window.document.querySelectorAll('img').forEach((img) => {imageSrcs = [...imageSrcs, img.src];});window.ReactNativeWebView.postMessage(JSON.stringify(imageSrcs));})()`}
         onMessage={handleOnMessage}
       />
-      <Text>Cat-o-the-day App</Text>
+      <View>
+        <Text>Cat-o-the-day App</Text>
+        <Button
+          title="Show Me Cat!!"
+          onPress={() => setDisplayedImage(images[Math.floor(Math.random() * images.length)])}
+        />
+      </View>
 
-      {imageUri ? <ImageContainer imageUri={imageUri} /> : null}
+      {displayedImage ? <ImageContainer imageUri={displayedImage} /> : null}
+      <StatusBar style="auto" />
     </View>
   );
 }
