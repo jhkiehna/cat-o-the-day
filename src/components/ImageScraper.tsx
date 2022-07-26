91614071d4172e1b7bcbf12bfed684c646f5aa78
minty <jhkieha@gmail.com>
2022-07-25 22:55:31 -0400

    adding type prop and search param

--- src/components/ImageScraper.tsx
+++ src/components/ImageScraper.tsx
@@ -2,17 +2,24 @@ import { Platform } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { WebViewMessageEvent } from 'react-native-webview/lib/WebViewTypes';
 
-export default function ImageScraper(props: { style: object; handleOnMessage: (event: WebViewMessageEvent) => void }) {
-  const { style, handleOnMessage } = props;
-
+export default function ImageScraper(props: {
+  style: object;
+  handleOnMessage: (event: WebViewMessageEvent) => void;
+  type: string;
+}) {
   if (Platform.OS === 'web') return null;
+
+  const { style, handleOnMessage, type } = props;
 
   return (
     <WebView
       style={style}
-      source={{ uri: 'https://www.google.com/search?q=cats&tbm=isch&tbs=isz:m' }}
-      injectedJavaScript={`(() => {let imageSrcs = []; window.document.querySelectorAll('img').forEach((img) => {imageSrcs = [...imageSrcs, img.src];});window.ReactNativeWebView.postMessage(JSON.stringify(imageSrcs));})()`}
+      source={{ uri: `https://www.google.com/search?q=cats+${type}&tbm=isch&tbs=isz:m` }}
+      scalesPageToFit={false}
+      contentMode="desktop"
+      injectedJavaScript={`(() => {let imageSrcs = []; window.document.querySelectorAll('img').forEach((img) => {if(img.width > 100 && img.src.length) imageSrcs = [...imageSrcs, img.src];}); window.ReactNativeWebView.postMessage(JSON.stringify(imageSrcs));})()`}
       onMessage={handleOnMessage}
+      onError={(event) => console.error(event.nativeEvent)}
     />
   );
 }
