9f01890b1f31d7ab0cf893796728e306231fd259
minty <jhkieha@gmail.com>
2022-07-26 21:32:28 -0400

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button, TextInput, Platform } from 'react-native';
-import { WebViewMessageEvent } from 'react-native-webview/lib/WebViewTypes';
-import { debounce } from 'lodash';
+import { Text, View, Button, TextInput } from 'react-native';
+import { debounce, random } from 'lodash';
 
 import { styles } from 'styles';
 import { ImageContainer, ImageScraper } from 'components';
-import { fetchCatsFallback } from 'utils';
 
-const defaultTypes = [
+const defaultModifiers = [
   'cute',
   'playing',
   'baby',
@@ -26,58 +24,58 @@ const defaultTypes = [
 ];
 
 function App() {
+  const initialModifier = defaultModifiers[random(0, defaultModifiers.length - 1)];
+
   const [displayedImage, setDisplayedImage] = React.useState<string>(null);
   const [images, setImages] = React.useState<string[]>([]);
-  const [type, setType] = React.useState<string>(defaultTypes[Math.floor(Math.random() * defaultTypes.length)]);
+  const [inputText, setInputText] = React.useState<string>(initialModifier);
+  const [modifier, setModifier] = React.useState<string>(initialModifier);
+  const updateModifier = React.useCallback(
+    debounce((text) => {
+      setModifier(text);
+    }, 500),
+    [],
+  );
 
-  function handleOnMessage(event: WebViewMessageEvent) {
-    if (images.length) return; // This fires twice for some reason, returning early if we already have images
+  function getRandomNewImage() {
+    let randomImage: string;
 
-    let imageSrcs = JSON.parse(event.nativeEvent.data) as string[];
-    console.log({ imageSrcs });
+    do {
+      randomImage = images[random(0, images.length - 1)];
+    } while (displayedImage === randomImage && displayedImage?.length);
 
-    setImages(imageSrcs);
-
-    if (!displayedImage) setDisplayedImage(imageSrcs[Math.floor(Math.random() * imageSrcs.length)]);
+    return randomImage;
   }
+
+  React.useEffect(() => {
+    if (images.length) setDisplayedImage(getRandomNewImage());
+  }, [images]);
 
   async function handleClick() {
-    if (images.length) return setDisplayedImage(images[Math.floor(Math.random() * images.length)]);
-
-    if (Platform.OS === 'web') {
-      const fallbackImages = await fetchCatsFallback();
-      setImages(fallbackImages);
-
-      if (!displayedImage && fallbackImages.length) {
-        setDisplayedImage(fallbackImages[Math.floor(Math.random() * fallbackImages.length)]);
-      }
-    }
+    if (images.length) setDisplayedImage(getRandomNewImage());
   }
 
-  function handleChange(event) {
-    setImages([]);
-    setDisplayedImage(null);
-
-    return setType(event);
+  function handleChangeText(text: string) {
+    setInputText(text);
+    updateModifier(text);
   }
 
   return (
     <View style={styles.container}>
-      <ImageScraper style={{ display: 'none', width: 0, height: 0 }} handleOnMessage={handleOnMessage} type={type} />
-      <Text
-        style={styles.header}
-        numberOfLines={1}
-        allowFontScaling={true}
-        adjustsFontSizeToFit={true}
-        minimumFontScale={0.1}
-        suppressHighlighting={true}
-      >
+      <View style={{ display: 'none', width: 0, height: 0 }}>
+        <ImageScraper setImages={setImages} modifier={modifier} />
+      </View>
+
+      <Text style={styles.header} suppressHighlighting={true}>
         Cat-o-the-Day!!
       </Text>
-      <TextInput style={styles.input} value={type} onChangeText={handleChange} />
+
+      <TextInput style={styles.input} value={inputText} onChangeText={handleChangeText} />
+
       <Button title="Show Me Cat!!" onPress={handleClick} />
 
       {displayedImage ? <ImageContainer imageUri={displayedImage} /> : null}
+
       <StatusBar style="auto" />
     </View>
   );
