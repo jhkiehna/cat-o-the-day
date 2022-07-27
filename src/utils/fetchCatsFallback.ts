cb51cc8021c5413ca6440bd93c12a21e7a594330
minty <jhkieha@gmail.com>
2022-07-26 21:31:17 -0400

    updated fallback to take a modifier

--- src/utils/fetchCatsFallback.ts
+++ src/utils/fetchCatsFallback.ts
@@ -1,9 +1,9 @@
 const pexelsApiKey = '563492ad6f917000010000017b40f39d8095444facc21e9d744ba5b0';
 
-export default async function fetchCatsFallback(): Promise<string[]> {
+export default async function fetchCatsFallback(modifier: string): Promise<string[]> {
   let imageSrcs: string[] = [];
 
-  const response = await fetch('https://api.pexels.com/v1/search?query=cat', {
+  const response = await fetch(`https://api.pexels.com/v1/search?query=cat${modifier ? `+${modifier}` : ''}`, {
     headers: { Authorization: pexelsApiKey },
     mode: 'cors',
   }).catch((error) => console.error(error));
