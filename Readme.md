2e6669d2fe8265437c6cfd14ad855c950ca1327a
minty <jhkieha@gmail.com>
2022-08-08 23:40:57 -0400

    Adding React native tailwind

---
+++ Readme.md
@@ -0,0 +1,8 @@
+#Running Locally
+
+- npm install
+
+Run both of these commands in parrellel (One builds and watches tailwind stuff)
+
+- npm run dev:tailwind
+- npm run start
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: require('tailwind-rn/unsupported-core-plugins'),
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
