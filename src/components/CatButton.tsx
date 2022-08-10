2f5cc8248199e61efbb1632ae4b6fd44d6348fc6
minty <jhkieha@gmail.com>
2022-08-09 20:12:51 -0400

    adding a custom Cat Button component with some custom styling

---
+++ src/components/CatButton.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  Platform,
+  TouchableNativeFeedback,
+  TouchableNativeFeedbackProps,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  GestureResponderEvent,
+  View,
+  Image,
+  ImageBackground,
+  Text,
+} from 'react-native';
+import { useTailwind } from 'tailwind-rn';
+
+type CatButtonProps = {
+  text: string;
+  onPress: (event?: GestureResponderEvent) => void;
+};
+
+const CatButton: React.FC<CatButtonProps> = ({ text, onPress }) => {
+  const tailwind = useTailwind();
+
+  const Touchable: React.ComponentType<TouchableOpacityProps | TouchableNativeFeedbackProps> =
+    Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+
+  return (
+    <Touchable onPress={onPress} accessibilityRole="button">
+      <View style={tailwind('h-10 flex justify-center bg-sky-200 static')}>
+        <ImageBackground
+          source={require('images/cat-paw-vector-19.png')}
+          style={tailwind('absolute top-0 left-0 w-2/3 h-full opacity-50')}
+          resizeMode="cover"
+          blurRadius={2}
+        />
+        <ImageBackground
+          source={require('images/cat-paw-vector-19.png')}
+          style={{
+            ...tailwind('absolute bottom-0 right-0 w-1/2 h-full opacity-50'),
+            transform: [{ scaleX: -1 }, { scaleY: -1 }],
+          }}
+          resizeMode="cover"
+          blurRadius={2}
+        />
+        <Text
+          style={{
+            ...tailwind('text-center font-bold text-lg'),
+            textShadowColor: '#333',
+            textShadowRadius: 1,
+            textShadowOffset: { width: 0.5, height: 0.5 },
+            shadowOpacity: 0.2,
+            opacity: 1,
+          }}
+        >
+          {text.toUpperCase()}
+        </Text>
+      </View>
+    </Touchable>
+  );
+};
+
+export default CatButton;
