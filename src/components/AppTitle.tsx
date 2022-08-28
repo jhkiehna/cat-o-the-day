fe2c047980043c9fefd0f33663ec0e193f06016f
Henry Kiehna <33327590+jhkiehna@users.noreply.github.com>
2022-08-28 10:51:28 -0400

    Update/ios changes (#11)
    * fixing font problem
    
    * there's some different in how useEffects fire between ios and anything else. Removed the use effect with images as dependency and went to just a callback function
    
    * status bar was being over/underlaid by the app, setting transluscent to false so that the status bar can be seen
    
    * adding keyboard avoiding view for ios
    
    * moving the statusBar out of the main view and explicitly setting a height so that it doesnt ever overlap with the main view when the keyboard opens and closes
    
    Co-authored-by: Henry Kiehna <jhkieha@gmail.com>

--- src/components/AppTitle.tsx
+++ src/components/AppTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, Platform } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 
-const iOSFont = 'AvenirNext-Bold, Avenir Next, sans-serif-medium, sans-serif';
+const iOSFont = 'AvenirNext-Bold';
 const fontFamily = Platform.OS === 'ios' ? iOSFont : 'sans-serif';
 
 const AppTitle: React.FC<{ text: string }> = ({ text }) => {
