1a07990447fd68ae4e94b0f53cace6a10c00d03c
Henry Kiehna <33327590+jhkiehna@users.noreply.github.com>
2022-08-28 11:59:17 -0400

    Fix/ios again (#15)
    * updating package for expo
    
    * moving status bar outside the main view. Setting a backgorund color that makes the text visibile with both white and black text, setting padding top . tired of messing with this
    
    Co-authored-by: Henry Kiehna <jhkieha@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { registerRootComponent } from 'expo';
+import { View, Platform } from 'react-native';
+import Constants from 'expo-constants';
+import { StatusBar } from 'expo-status-bar';
 import { TailwindProvider } from 'tailwind-rn';
 import utilities from '../tailwind.json';
 
@@ -7,7 +10,15 @@ import Home from './pages/Home';
 function App() {
   return (
     <TailwindProvider utilities={utilities}>
-      <Home />
+      <View
+        style={{
+          backgroundColor: 'rgb(186, 230, 253)',
+          paddingTop: Platform.OS === 'ios' ? Constants.statusBarHeight : 0,
+        }}
+      >
+        <StatusBar style="dark" />
+        <Home />
+      </View>
     </TailwindProvider>
   );
 }
